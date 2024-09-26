444fc81430f149f7ae875a173d5b8a102d10df84
TV do BILL <102390981+listadobill@users.noreply.github.com>
2024-09-26 20:32:27 -0300

    Update proxy.php

--- docs/proxy.php
+++ docs/proxy.php
@@ -1,4 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain"); // Define o tipo de conteúdo como texto
 echo file_get_contents("http://jmrk.xyz:8880/get.php?username=scott_olson@hotmail.com&password=2p7Ks4SPxP&type=m3u");
 ?>
